2569aeeac7ffd1de932ab1e2e5cb36b89093f9df
Anton <itonyemail@gmail.com>
2023-12-08 15:52:13 -0800

    awake added

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -1,5 +1,10 @@
 using UnityEngine;
 
 public class Clock : MonoBehaviour{
+    [SerializeField]
+    Transform hoursPivot, minutesPivot, secondsPivot;
 
+    void Awake () {
+        Quaternion.Euler;
+    }
 }
