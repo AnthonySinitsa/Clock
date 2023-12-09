25f3591acf33c9671ed94a60c6c8bdf17eee93cc
Anton <itonyemail@gmail.com>
2023-12-08 17:31:02 -0800

    Update Clock.cs

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 
 
 public class Clock : MonoBehaviour{
-    float hoursToDegrees = -30
+    const float hoursToDegrees = -30f, minutesToDegrees = -6f, secondsToDegrees = -6f;
     [SerializeField]
     Transform hoursPivot, minutesPivot, secondsPivot;
 
     void Awake () {
-        hoursPivot.localRotation = Quaternion.Euler(0, 0, hoursToDegrees * DateTime.Now.Hour);
+        hoursPivot.localRotation = Quaternion.Euler(0f, 0f, hoursToDegrees * DateTime.Now.Hour);
+        minutesPivot.localRotation = Quaternion.Euler(0f, 0f, minutesToDegrees * DateTime.Now.Minute);
+        secondsPivot.localRotation = Quaternion.Euler(0f, 0f, secondsToDegrees * DateTime.Now.Second);
     }
 }
